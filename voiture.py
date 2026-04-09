e335c9068ed808ddeec280077458782ad519a872
mansouri mohamed <mns09moh@gmail.com>
2026-04-09 15:42:42 -0400

    creation class voiture

---
+++ voiture.py
@@ -0,0 +1,8 @@
+class Voiture:
+    def _init_(self, marque, modele, annee, prix, id=None):
+        self.id = id
+        self.marque = marque
+        self.modele = modele
+        self.annee = annee
+        self.prix = prix
+
