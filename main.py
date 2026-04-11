0ffac3cbd8fad7d6f989a3a302924e6959580587
mansouri mohamed <mns09moh@gmail.com>
2026-04-10 23:37:00 -0400

    Ajout du test d insertion de plusieurs voitures

--- main.py
+++ main.py
@@ -12,3 +12,19 @@ def tester_connexion():
         print("Connexion fermée.")
     else:
         print("Échec de la connexion.")
+def tester_ajout():
+    print("\n=== TEST AJOUT ===")
+    v1 = Voiture("Toyota", "Corolla", 2020, 18500.00)
+    v2 = Voiture("Honda", "Civic", 2019, 17200.50)
+    v3 = Voiture("Ford", "Focus", 2018, 14999.99)
+
+    ajouter_voiture(v1)
+    ajouter_voiture(v2)
+    ajouter_voiture(v3)
+
+    print("Voitures ajoutées :")
+    v1.afficher_voiture()
+    v2.afficher_voiture()
+    v3.afficher_voiture()
+
+    return v1, v2, v3
